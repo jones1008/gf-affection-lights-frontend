df184ee9ff42bf1ab2fabda3133e1df0a083dce8
Jonas Fluhrer <jonas.fluhrer@novatec-gmbh.de>
2023-10-07 14:54:38 +0200

    send API call

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import './App.css';
 type LampState = "OFF" | "TURNING-ON" | "ON";
 
 function App() {
-  let [lampState, setLampState] = useState<LampState>("OFF");
+    let [lampState, setLampState] = useState<LampState>("OFF");
 
     let src: string;
     switch (lampState) {
@@ -19,24 +19,70 @@ function App() {
             break;
     }
 
-  const changeLamp = () => {
-      if (lampState === 'OFF') {
-          setLampState('TURNING-ON');
-          setTimeout(() => {
-              setLampState("ON");
-              setTimeout(() => {
-                  setLampState("OFF");
-              }, 4000);
-          }, 2000);
-      }
-  };
-  return (
-      <>
-        <h4>Du brauchst Aufmerksamkeit von Jonesi?</h4>
-        <h3>Klick die Glühbirne!</h3>
-        <img alt={'lamp ' + lampState.toLowerCase()} src={src} onClick={changeLamp}/>
-      </>
-  );
+    let [errorMessage, setErrorMessage] = useState<string>("");
+    let [successMessage, setSuccessMessage] = useState<string>("");
+
+    const sendSms = () => {
+        const url = new URL(`${process.env.REACT_APP_BACKEND_URL}/send-affection-sms`);
+        url.searchParams.set('message', 'affection please!');
+        let headers;
+        console.log(process.env.NODE_ENV);
+        if (process.env.NODE_ENV === 'production') {
+            headers = {
+                "Access-Control-Allow-Origin": "*"
+            }
+        }
+        return fetch(url, { headers });
+    };
+
+    const changeLamp = () => {
+        if (lampState === 'OFF') {
+            setLampState('TURNING-ON');
+            sendSms().then((res) => {
+                if (res.ok) {
+                    setLampState("ON");
+
+                    setSuccessMessage('Affection Light leuchtet <3');
+                    setTimeout(() => {
+                        setSuccessMessage("");
+                    }, 8000);
+
+                    setTimeout(() => {
+                        setLampState("OFF");
+                    }, 10000);
+                } else {
+                    setLampState("OFF");
+                    setErrorMessage('Bitte warte 2 Minuten...');
+                    setTimeout(() => {
+                        setErrorMessage("");
+                    }, 5000);
+                }
+            }).catch(() => {
+                setErrorMessage('Etwas ist schiefgelaufen :(');
+                setTimeout(() => {
+                    setErrorMessage("");
+                }, 5000);
+            });
+        }
+    };
+
+    return (
+        <>
+            <h4>Du brauchst Aufmerksamkeit von Jonesi?</h4>
+            <h3>Klick die Glühbirne!</h3>
+            <img alt={'lamp ' + lampState.toLowerCase()} src={src} onClick={changeLamp}/>
+            {errorMessage &&
+                <div className='snackbar error-message'>
+                    {errorMessage}
+                </div>
+            }
+            {successMessage &&
+                <div className='snackbar success-message'>
+                    {successMessage}
+                </div>
+            }
+        </>
+    );
 }
 
 export default App;
